70e53dd83bc30ee0ecd56a98973d6b7ec44b32cf
Pavit Pimchanagul <pavitpim@gmail.com>
2023-02-07 23:51:50 +0700

    100% with remove comment

--- mandatory_9/get_next_line.c
+++ mandatory_9/get_next_line.c
@@ -1,6 +1,6 @@
 #include "get_next_line.h"
 
-// ########### NOT FREE STORAGE
+
 char *extract_full_line(char *storage, int index)
 {
 
@@ -10,8 +10,6 @@ char *extract_full_line(char *storage, int index)
   i = 0;
 
   full_line = malloc(sizeof(char) * index + 1 + 1);
-  //  printf("FULLINE ADDRESS IS %p\n", full_line);
-  // printf("SIZE OF FULLINE  %d\n", index + 1 + 1);
   if (!full_line)
     return NULL;
 
@@ -28,7 +26,7 @@ char *extract_full_line(char *storage, int index)
   return full_line;
 }
 
-// ########### NOT FREE STORAGE
+
 char *extract_last_line(char *storage)
 {
   int len;
@@ -37,18 +35,12 @@ char *extract_last_line(char *storage)
 
   len = ft_strlen(storage);
   i = 0;
-  // printf("length of storage is %d\n",len);
+ 
   if (len == 0)
-  {
-    // printf("EMPTY STORAGE");
     return NULL;
-  }
+  
 
   full_line = malloc(sizeof(char) * (len + 1));
-  // printf("LAST LINE ADDRESS IS %p\n", full_line);
-  // printf("SIZE OF LAST LINE  %d\n", len + 1);
-  //  printf("ADDRESS IS   %p\n", full_line);
-  // printf("SIZE OF IS   %d\n", len+1);
 
   if (!full_line)
     return NULL;
@@ -63,7 +55,6 @@ char *extract_last_line(char *storage)
   return full_line;
 }
 
-// ###########  FREE STORAGE
 char *truncate_storage(char *storage, int index)
 {
   int len;
@@ -79,10 +70,6 @@ char *truncate_storage(char *storage, int index)
   i = 0;
 
   new_storage = malloc(sizeof(char) * (len - index));
-  // printf("NEW STORAGE ADDRESS IS %p\n", new_storage);
-  // printf("SIZE OF NEW STORAGE  %d\n", len - index);
-  // printf("ADDRESS IS   %p\n", new_storage);
-  // printf("SIZE OF IS   %d\n", len-index);
 
   if (!new_storage)
     return NULL;
@@ -101,7 +88,7 @@ char *truncate_storage(char *storage, int index)
   return new_storage;
 }
 
-// ###########  FREE STORAGE
+
 char *storage_join(char *storage, char *buffer)
 {
   char *new_storage;
@@ -112,15 +99,9 @@ char *storage_join(char *storage, char *buffer)
 
   len1 = ft_strlen(storage);
   len2 = ft_strlen(buffer);
-  //  printf("JOIN : OLD STORAGE ADDRESS IS %p\n", storage);
-  //  printf("JOIN : BUFFER  ADDRESS IS %p\n", buffer);
   new_storage = malloc(len1 + len2 + 1);
-  //  printf("JOIN : NEW STORAGE ADDRESS IS %p\n", new_storage);
-  // printf("JOIN : SIZE OF NEW STORAGE  %lu\n", len1 + len2 + 1);
-
   if (!new_storage)
     return NULL;
-
   i = 0;
   while (storage && storage[i])
   {
@@ -153,68 +134,28 @@ char *get_next_line(int fd)
   buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
   if (!buffer)
     return NULL;
-  // printf("ADD OF BUFF %p\n", buffer);
-  // printf("ADD OF BUFF %p\n", BUFFER_SIZE + 1);
+
   index = -1;
   read_byte = 1;
   while (read_byte > 0 && index == -1)
   {
-    read_byte = (int)read(fd, buffer, BUFFER_SIZE); // 217
-    buffer[read_byte] = '\0';
-    // printf("BUFFER LEN IS %zd\n",ft_strlen(buffer));
-    //  printf("ADDRESS OF BUFFER %p\n", buffer);
-    storage = storage_join(storage, buffer);
-    // printf("ADDRESS OF STORAGE %p\n", storage);
+    read_byte = (int)read(fd, buffer, BUFFER_SIZE); 
+    if(read_byte!=0 && read_byte != -1)
+    {
+      buffer[read_byte] = '\0';
+      storage = storage_join(storage, buffer);
+    }
     index = ft_char_index(storage, '\n');
-    // printf("\nREAD BYTE IS %d\n",read_byte);
-    // printf("INDEX IS %d\n",index);
   }
-  //  printf("BEF : address of buffer %p\n",buffer);
-  // printf("BEF : value of buffer %s\n", buffer);
-  // printf("BEF : buffer length %zu\n\n", ft_strlen(buffer));
   free(buffer);
-  // buffer = NULL;
-  // printf("AF : address of buffer %p\n",buffer);
-  // printf("AF : value of buffer %s\n", buffer);
-  // printf("AF : buffer length %zu\n", ft_strlen(buffer));
   if (index == -1)
   {
     buffer = extract_last_line(storage);
-    //  printf("ADDRESS #3 %p\n",storage);
-    //  printf("\n STORAGE LENGTH %zu\n",ft_strlen(storage));
     free(storage);
-    //  printf("ADDRESS #4 %p\n",storage);
-
     storage = NULL;
     return buffer;
   }
   buffer = extract_full_line(storage, index);
-  // printf("ADDRESS %p\n",storage);
   storage = truncate_storage(storage, index);
-  //  printf("\n STORAGE LENGTH %zu\n",ft_strlen(storage));
   return buffer;
 }
-
-
-// int main()
-// {
-//     char *string;
-//     int fd;
-// 	int i = 0;
-
-//     fd = open("text.txt", O_RDONLY);
-
-// 	while (i < 11)
-// 	{
-//      printf("LOOP AT %d\n",i+1);
-//     	string = get_next_line(fd);
-//     	// string = get_next_line(fd);
-// 		printf("%s", string);
-   
-// 		i++;
-// 	}
-  
-// 	free(string);
-//     close(fd);
-//     return (0);
-// }
